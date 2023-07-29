9eccbda2f24b485b90dd79f232b2753b62b2d123
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2023-07-29 16:14:05 -0400

    Initial plugin

---
+++ BSUniTaskPlugin/Plugin.cs
@@ -0,0 +1,60 @@
+﻿using Cysharp.Threading.Tasks;
+using IPA;
+using UnityEngine;
+using UnityEngine.LowLevel;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BSUniTaskPlugin
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+            Log.Info("BSUniTaskPlugin initialized.");
+            
+            var loop = PlayerLoop.GetCurrentPlayerLoop();
+            PlayerLoopHelper.Initialize(ref loop);
+            
+            Debug.Log($"UniTaskPlayerLoop ready? {PlayerLoopHelper.IsInjectedUniTaskPlayerLoop().ToString()}");
+            PlayerLoopHelper.DumpCurrentPlayerLoop();
+        }
+
+        #region BSIPA Config
+
+        //Uncomment to use BSIPA's config
+        /*
+        [Init]
+        public void InitWithConfig(Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded");
+        }
+        */
+
+        #endregion
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+        }
+    }
+}
